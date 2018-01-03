901b7d0c75162ee797ad4c4b30a396f37c02569d
Antonio Fontaine <afontaine@uc.cl>
2018-01-03 13:10:05 -0300

    mchanges in fx_descriptor, saving images correctly

--- fx_descriptor.m
+++ fx_descriptor.m
@@ -21,13 +21,11 @@ res = not(strcmp(op.res,'nothing')); % 'nothing' means no restoration
 
 
 % all 100 images op.img of AR are extracted
-sq = ['_' num2fixstr(op.img,op.imgdig) '.png'];
 
 folder = [op.fpath num2fixstr(op.img,op.imgdig) '/' op.prefix];
-disp(folder);
+
 a = dir([folder '/*.png']);
 N  = size(a,1); % numer of images
-disp(N);
 
 % if op.ini==1 % op.ini must be 1 to extract the features
 %     XX = zeros(N,op.size);
@@ -42,49 +40,45 @@ end
 
 ft = Bio_statusbar('extracting features');
 for i=1:N
-    for j=1:12
-        ft = Bio_statusbar(i/N,ft);
-        if op.ini
-            % read image op.img of subject i
-            st = [op.fpath op.prefix num2fixstr(i,op.subdig) '_' num2fixstr(j,2)  sq];
-            if(exist(st) == 2)
-                I = imread(st);
-                if op.gray == 1
-                    if size(I,3) == 3
-                        I = rgb2gray(I);
-                    end
-                end
-                % first image is displayed
-                if i==1
-                    figure(1);imshow(I,[]);title('original');
-                end
-                % in case of degradation
-                if deg
-                    I = im_preprocessing(I,op.deg,opdeg);
-                    if i==1
-                        figure(2);imshow(I,[]);title('degraded');
-                    end
-                end
-                % in case of restoration
-                if res
-                    I = im_preprocessing(I,op.res,opres);
-                    if i==1
-                        figure(3);imshow(I,[]);title('restored');
-                    end
-                end
-                % row i contains the descriptrion of image op.img of subject i
-                % the description is extracted using function op.fx_function with
-                % parameters opfx
-                x = feval(op.fx_function,I,opfx);
-                if op.size == 0 %% QUE ES ESTO
-                    op.size = length(x);
-                    XX = zeros(N,op.size);
-                    fprintf('descriptor has %d elements.\n',op.size);
-                end
-                XX(i, :) = x;
-
+    ft = Bio_statusbar(i/N,ft);
+    
+    if op.ini
+        % read image op.img of subject i
+        st = [folder num2fixstr(i,op.subdig) '.png'];
+        I = imread(st);
+        if op.gray == 1
+            if size(I,3) == 3
+                I = rgb2gray(I);
             end
         end
+        % first image is displayed
+        if i==1
+            figure(1);imshow(I,[]);title('original');
+        end
+        % in case of degradation
+        if deg
+            I = im_preprocessing(I,op.deg,opdeg);
+            if i==1
+                figure(2);imshow(I,[]);title('degraded');
+            end
+        end
+        % in case of restoration
+        if res
+            I = im_preprocessing(I,op.res,opres);
+            if i==1
+                figure(3);imshow(I,[]);title('restored');
+            end
+        end
+        % row i contains the descriptrion of image op.img of subject i
+        % the description is extracted using function op.fx_function with
+        % parameters opfx
+        x = feval(op.fx_function,I,opfx);
+        if op.size == 0 %% QUE ES ESTO
+            op.size = length(x);
+            XX = zeros(N,op.size);
+            fprintf('descriptor has %d elements.\n',op.size);
+        end
+        XX(i, :) = x;    
     end
 end
 delete(ft)
